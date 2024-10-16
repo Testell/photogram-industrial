5988c1efcdf5544703e49894cec9c841bb07c2c3
Tyler Estell <108036777+Testell@users.noreply.github.com>
2024-10-16 20:41:40 +0000

    added nav links to user profile, for posts and liked photos

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   resources :likes
   resources :photos
 
+  get "/:username/liked" => "users#liked", as: :liked
+
   get "/:username" => "users#show", as: :user
 end
