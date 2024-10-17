1618cbf8e29e82e851875a86e4acb8618e468d0a
Tyler Estell <108036777+Testell@users.noreply.github.com>
2024-10-17 03:13:06 +0000

    added feed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,9 @@ class UsersController < ApplicationController
   def liked
     @user = User.find_by!(username: params.fetch(:username))
   end
+
+  def feed
+    @user = User.find_by!(username: params.fetch(:username))
+    @feed = Photo.where(owner_id: @user.leaders.pluck(:id)).order(created_at: :desc)
+  end
 end
